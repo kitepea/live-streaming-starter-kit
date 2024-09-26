637a501aa592150dcc5972a411d8ce34f898f323
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-26 18:35:41 +0700

    Update microphone_v2.py

--- microphone_v2.py
+++ microphone_v2.py
@@ -1,71 +1,146 @@
-from deepgram import Deepgram
-from deepgram.transcription import LiveTranscriptionEvents, LiveOptions
-from microphone import Microphone
+# Copyright 2023-2024 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
 
-deepgram = Deepgram('YOUR_DEEPGRAM_API_KEY')
+from time import sleep
+import logging
 
-dg_connection = deepgram.transcription.live()
+from deepgram.utils import verboselogs
 
-
-def on_open(self, open, **kwargs):
-    print(f"\n\n{open}\n\n")
-
-def on_message(self, result, **kwargs):
-    sentence = result.channel.alternatives[0].transcript
-    if len(sentence) == 0:
-        return
-    print(f"speaker: {sentence}")
-
-def on_metadata(self, metadata, **kwargs):
-    print(f"\n\n{metadata}\n\n")
-
-def on_speech_started(self, speech_started, **kwargs):
-    print(f"\n\n{speech_started}\n\n")
-
-def on_utterance_end(self, utterance_end, **kwargs):
-    print(f"\n\n{utterance_end}\n\n")
-
-def on_error(self, error, **kwargs):
-    print(f"\n\n{error}\n\n")
-
-def on_close(self, close, **kwargs):
-    print(f"\n\n{close}\n\n")
-
-dg_connection.on(LiveTranscriptionEvents.Open, on_open)
-dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
-dg_connection.on(LiveTranscriptionEvents.Metadata, on_metadata)
-dg_connection.on(LiveTranscriptionEvents.SpeechStarted, on_speech_started)
-dg_connection.on(LiveTranscriptionEvents.UtteranceEnd, on_utterance_end)
-dg_connection.on(LiveTranscriptionEvents.Error, on_error)
-dg_connection.on(LiveTranscriptionEvents.Close, on_close)
-
-options: LiveOptions = LiveOptions(
-    model="nova-2",
-    punctuate=True,
-    language="vi",
-    encoding="linear16",
-    channels=1,
-    sample_rate=16000,
-    ## To get UtteranceEnd, the following must be set:
-    interim_results=True,
-    utterance_end_ms="1000",
-    vad_events=True,
+from deepgram import (
+    DeepgramClient,
+    DeepgramClientOptions,
+    LiveTranscriptionEvents,
+    LiveOptions,
+    Microphone,
 )
-dg_connection.start(options)
 
-## create microphone
-microphone = Microphone(dg_connection.send)
 
-## start microphone
-microphone.start()
+# We will collect the is_final=true messages here so we can use them when the person finishes speaking
+is_finals = []
 
-## wait until finished
-input("Press Enter to stop recording...\n\n")
 
-## Wait for the microphone to close
-microphone.finish()
+def main():
+    try:
+        # example of setting up a client config. logging values: WARNING, VERBOSE, DEBUG, SPAM
+        # config = DeepgramClientOptions(
+        #     verbose=verboselogs.DEBUG, options={"keepalive": "true"}
+        # )
+        # deepgram: DeepgramClient = DeepgramClient("", config)
+        # otherwise, use default config
+        deepgram: DeepgramClient = DeepgramClient("de501bf00821a2c8a41fa97d3836021f96effbd3")
 
-## Indicate that we've finished
-dg_connection.finish()
+        dg_connection = deepgram.listen.websocket.v("1")
 
-print("Finished")
+        def on_open(self, open, **kwargs):
+            print("Connection Open")
+
+        def on_message(self, result, **kwargs):
+            global is_finals
+            sentence = result.channel.alternatives[0].transcript
+            if len(sentence) == 0:
+                return
+            if result.is_final:
+                print(f"Message: {result.to_json()}")
+                # We need to collect these and concatenate them together when we get a speech_final=true
+                # See docs: https://developers.deepgram.com/docs/understand-endpointing-interim-results
+                is_finals.append(sentence)
+
+                # Speech Final means we have detected sufficent silence to consider this end of speech
+                # Speech final is the lowest latency result as it triggers as soon an the endpointing value has triggered
+                if result.speech_final:
+                    utterance = " ".join(is_finals)
+                    print(f"Speech Final: {utterance}")
+                    is_finals = []
+                else:
+                    # These are useful if you need real time captioning and update what the Interim Results produced
+                    print(f"Is Final: {sentence}")
+            else:
+                # These are useful if you need real time captioning of what is being spoken
+                print(f"Interim Results: {sentence}")
+
+        def on_metadata(self, metadata, **kwargs):
+            print(f"Metadata: {metadata}")
+
+        def on_speech_started(self, speech_started, **kwargs):
+            print("Speech Started")
+
+        def on_utterance_end(self, utterance_end, **kwargs):
+            print("Utterance End")
+            global is_finals
+            if len(is_finals) > 0:
+                utterance = " ".join(is_finals)
+                print(f"Utterance End: {utterance}")
+                is_finals = []
+
+        def on_close(self, close, **kwargs):
+            print("Connection Closed")
+
+        def on_error(self, error, **kwargs):
+            print(f"Handled Error: {error}")
+
+        def on_unhandled(self, unhandled, **kwargs):
+            print(f"Unhandled Websocket Message: {unhandled}")
+
+        dg_connection.on(LiveTranscriptionEvents.Open, on_open)
+        dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
+        dg_connection.on(LiveTranscriptionEvents.Metadata, on_metadata)
+        dg_connection.on(LiveTranscriptionEvents.SpeechStarted, on_speech_started)
+        dg_connection.on(LiveTranscriptionEvents.UtteranceEnd, on_utterance_end)
+        dg_connection.on(LiveTranscriptionEvents.Close, on_close)
+        dg_connection.on(LiveTranscriptionEvents.Error, on_error)
+        dg_connection.on(LiveTranscriptionEvents.Unhandled, on_unhandled)
+
+        options: LiveOptions = LiveOptions(
+            model="nova-2",
+            language="vi",
+            # Apply smart formatting to the output
+            smart_format=True,
+            # Raw audio format details
+            encoding="linear16",
+            channels=1,
+            sample_rate=16000,
+            # To get UtteranceEnd, the following must be set:
+            interim_results=True,
+            utterance_end_ms="1000",
+            vad_events=True,
+            # Time in milliseconds of silence to wait for before finalizing speech
+            endpointing=300,
+        )
+
+        addons = {
+            # Prevent waiting for additional numbers
+            "no_delay": "true"
+        }
+
+        print("\n\nPress Enter to stop recording...\n\n")
+        if dg_connection.start(options, addons=addons) is False:
+            print("Failed to connect to Deepgram")
+            return
+
+        # Open a microphone stream on the default input device
+        microphone = Microphone(dg_connection.send)
+
+        # start microphone
+        microphone.start()
+
+        # wait until finished
+        input("")
+
+        # Wait for the microphone to close
+        microphone.finish()
+
+        # Indicate that we've finished
+        dg_connection.finish()
+
+        print("Finished")
+        # sleep(30)  # wait 30 seconds to see if there is any additional socket activity
+        # print("Really done!")
+
+    except Exception as e:
+        print(f"Could not open socket: {e}")
+        return
+
+
+if __name__ == "__main__":
+    main()
